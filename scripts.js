fbfb0b90cd684895f28fab8e7f88073d8f68d111
Michael Rolf Onyango <rolfonyango@gmail.com>
2026-02-10 01:49:20 +0300

    Add a function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 // =====================
 // Business Logic
 // =====================
+
+function Place(location, landmarks, season, notes) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.season = season;
+  this.notes = notes;
+}
